04ae5174cb36d7eedf5811ad3117208e4507b383
alherd havartsou <alherd.havartsou@gmail.com>
2023-12-30 16:17:27 +0000

    'm

--- ERC20.sol
+++ ERC20.sol
@@ -5,7 +5,7 @@ import "./IERC20.sol";
 import "./IERC20Metadata.sol";
 import "./Context.sol";
 
-contract Bomp is Context, IERC20, IERC20Metadata {
+contract HST is Context, IERC20, IERC20Metadata {
     mapping(address => uint256) private _balances;
 
     mapping(address => mapping(address => uint256)) private _allowances;
